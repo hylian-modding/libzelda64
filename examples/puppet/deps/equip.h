fad3e03b42a06dfd80e014088b1e89c0c10612e1
CrookedPoe <djinsomniapro@gmail.com>
2021-03-30 03:00:00 -0500

    Equip Logic and Proxies
    Co-authored-by: den <denoflionsx@users.noreply.github.com>

--- examples/puppet/deps/equip.h
+++ examples/puppet/deps/equip.h
@@ -6,42 +6,82 @@
 
 Gfx LeftHandDisplay[];
 Gfx RightHandDisplay[];
-Gfx SwordBackDisplay[];
-Gfx ShieldBackDisplay[];
-Gfx CompositeBackDisplay[];
+Gfx BackDisplay[];
 
 static void ConstructEquipmentDisplay(entity_t* this, GlobalContext* globalCtx)
 {
+    uint8_t sw_idx = 0; /* Change this to the instance variable when it is added. */
+    uint8_t sh_idx = 0; /* Change this to the instance variable when it is added. */
 
-    if (this->sword < PLAYER_AP_SWORD_BGS)                                  /* Normal Sword */
+    uint32_t fists_l[] = {
+        ADULT_LINK_LUT_DL_LFIST
+        , CHILD_LINK_LUT_DL_LFIST
+    };
+
+    uint32_t fists_r[] = {
+        ADULT_LINK_LUT_DL_RFIST
+        , CHILD_LINK_LUT_DL_RFIST
+    };
+
+    uint32_t shields[] = {
+        PROXY_DL_DEKU_SHIELD
+        , PROXY_DL_HYLIAN_SHIELD
+        , PROXY_DL_MIRROR_SHIELD
+    };
+
+    uint32_t hilts[] = {
+        PROXY_DL_SWORD1_HILT
+        , PROXY_DL_SWORD2_HILT
+        , PROXY_DL_SWORD3_HILT
+    };
+
+    uint32_t blades[] = {
+        PROXY_DL_SWORD1_BLADE
+        , PROXY_DL_SWORD2_BLADE
+        , PROXY_DL_SWORD3_BLADE
+    };
+
+    uint32_t sheathes[] = {
+        PROXY_DL_SWORD1_SHEATH
+        , PROXY_DL_SWORD2_SHEATH
+        , PROXY_DL_SWORD3_SHEATH
+    };
+
+    /* Right Hand */
+    RightHandDisplay = (Gfx){
+        gsSPDisplayList(fists_r[this->age])
+        , gsSPBranchList(shields[sh_idx])
+    };
+
+    /* Left Hand */
+    LeftHandDisplay = (Gfx){
+        gsSPDisplayList(fists_l[this->age])
+        , gsSPDisplayList(hilts[sw_idx])
+        , gsSPBranchList(blades[sw_idx])
+    };
+
+    /* Equipment on Back */
+    if (0) /* Sword is pulled out */
     {
-        LeftHandDisplay = (Gfx){
-            gsSPDisplayList(RESOLVE_DL(this->age, 1, LFIST))                /* Left Fist */
-            , gsSPDisplayList(RESOLVE_DL(this->age, 1, SWORD_HILT))         /* Sword Handle */
-            , gsSPBranchList(RESOLVE_DL(this->age, 1, SWORD_BLADE))         /* Sword Blade */
+        BackDisplay = (Gfx){
+            gsSPDisplayList(sheathes[sw_idx])
+            , gsSPMatrix(0) /* Shield Matrix -- Rotation */
+            , gsSPDisplayList(shields[sh_idx])
+            , gsSPPopMatrix(1)
+            , gsSPEndDisplayList()
+        };
+    } else {
+        BackDisplay = (Gfx){
+            gsSPDisplayList(sheathes[sw_idx])
+            , gsSPMatrix(0) /* Hilt */
+            , gsSPDisplayList(hilts[sw_idx])
+            , gsSPPopMatrix(1)
+            , gsSPMatrix(0) /* Shield Matrix -- Rotation */
+            , gsSPDisplayList(shields[sh_idx])
+            , gsSPPopMatrix(1)
+            , gsSPEndDisplayList()
         };
     }
-    else if (!this->age)                                                    /* Adult Link */
-    {
-        if (this->sword > PLAYER_AP_SWORD_BGS)                              /* Broken Sword */
-        {
-            LeftHandDisplay = (Gfx){
-                gsSPDisplayList(RESOLVE_DL(this->age, 1, LFIST))                /* Left Fist */
-                , gsSPDisplayList(RESOLVE_DL(this->age, 1, LONGSWORD_HILT))     /* Sword Handle */
-                , gsSPBranchList(RESOLVE_DL(this->age, 1, LONGSWORD_BROKEN))    /* Sword Blade */
-            };
-        }
-        else                                                                /* Biggoron Sword */
-        {
-            LeftHandDisplay = (Gfx){
-                gsSPDisplayList(RESOLVE_DL(this->age, 1, LFIST))                /* Left Fist */
-                , gsSPDisplayList(RESOLVE_DL(this->age, 1, LONGSWORD_HILT))     /* Sword Handle */
-                , gsSPBranchList(RESOLVE_DL(this->age, 1, LONGSWORD_BLADE))    /* Sword Blade */
-            };
-        }
-    }
-
-
 }
 
 #endif /* __PUPPET_EQUIP_H__ */
