136aa3e66dc2d48b979e968ee1d94a00f395b943
Psi-Hate <dianaelzascudder@gmail.com>
2021-08-29 00:11:33 -0500

    Add some MM functions and fix SkelAnime_DrawFlexOpa

--- include/libzelda64/SkelAnime.h
+++ include/libzelda64/SkelAnime.h
@@ -17,33 +17,33 @@ typedef void (*PostLimbDrawOpa)(struct GlobalContext* globalCtx, int32_t limbInd
 
 extern int32_t SkelAnime_Init(
     struct GlobalContext* globalCtx, SkelAnime* skelAnime, SkeletonHeader* skeletonHeaderSeg, struct AnimationHeader* animation, Vec3s* limbDrawTable, Vec3s* morfDrawTable, int32_t limbCount);
-SYMBOL_VERSION_CONFIG(SkelAnime_Init, 0x8008C684, 0x80000180);
+SYMBOL_VERSION_CONFIG(SkelAnime_Init, 0x8008C684, 0x80136A7C);
 
 extern int32_t SkelAnime_InitFlex(
     struct GlobalContext* globalCtx, SkelAnime* skelAnime, FlexSkeletonHeader* skeletonHeaderSeg, struct AnimationHeader* animation, Vec3s* limbDrawTable, Vec3s* limbPosTable, int32_t limbCount);
-SYMBOL_VERSION_CONFIG(SkelAnime_InitFlex, 0x8008C788, 0x80000180);
+SYMBOL_VERSION_CONFIG(SkelAnime_InitFlex, 0x8008C788, 0x801385A0);
 
 // SkelAnime_InitLinkAnimation / Skeleton_Info_Rom_SV_ct on Poe's sheet (Decomp renamed, ovl didn't have)
 extern void SkelAnime_InitLink(
     struct GlobalContext* globalCtx, SkelAnime* skelAnime, FlexSkeletonHeader* skeletonHeaderSeg, struct LinkAnimationHeader* animation, int32_t initFlags, struct Vec3s* jointTable,
     struct Vec3s* morphTable, int32_t limbCount);
-SYMBOL_VERSION_CONFIG(SkelAnime_InitLink, 0x8008BB08, 0x80000180);
+SYMBOL_VERSION_CONFIG(SkelAnime_InitLink, 0x8008BB08, 0x80135F88);
 
 extern void SkelAnime_ChangeAnim(SkelAnime* skelAnime, struct AnimationHeader* animation, float playSpeed, float startFrame, float endFrame, uint8_t mode, float morphFrames);
-SYMBOL_VERSION_CONFIG(SkelAnime_ChangeAnim, 0x8008D17C, 0x80000180);
+SYMBOL_VERSION_CONFIG(SkelAnime_ChangeAnim, 0x8008D17C, 0x801373E8);
 
 extern void SkelAnime_DrawOpa(struct GlobalContext* globalCtx, void** skeleton, Vec3s* jointTable, OverrideLimbDrawOpa overrideLimbDraw, PostLimbDrawOpa postLimbDraw, void* instance);
-SYMBOL_VERSION_CONFIG(SkelAnime_DrawOpa, 0x8008993C, 0x80000180);
+SYMBOL_VERSION_CONFIG(SkelAnime_DrawOpa, 0x8008993C, 0x80133B3C);
 
 extern void SkelAnime_DrawFlexOpa(
     struct GlobalContext* globalCtx, void** skeleton, struct Vec3s* jointTable, int32_t dListCount, OverrideLimbDrawOpa overrideLimbDraw, PostLimbDrawOpa postLimbDraw, void* instance);
-SYMBOL_VERSION_CONFIG(SkelAnime_DrawFlexOpa, 0x8008993C, 0x80000180);
+SYMBOL_VERSION_CONFIG(SkelAnime_DrawFlexOpa, 0x80089D8C, 0x80133F28);
 
 extern void SkelAnime_DrawFlexLod(
     struct GlobalContext* globalCtx, void** skeleton, struct Vec3s* jointTable, int32_t dListCount, OverrideLimbDrawOpa overrideLimbDraw, PostLimbDrawOpa postLimbDraw, void* arg, int32_t dListIndex);
-SYMBOL_VERSION_CONFIG(SkelAnime_DrawFlexLod, 0x800894A4, 0x80000180);
+SYMBOL_VERSION_CONFIG(SkelAnime_DrawFlexLod, 0x800894A4, 0x80134DBC);
 
 extern int16_t Animation_GetLength(void* animation);
-SYMBOL_VERSION_CONFIG(Animation_GetLength, 0x8008A158, 0x80000180);
+SYMBOL_VERSION_CONFIG(Animation_GetLength, 0x8008A158, 0x80134748);
 
 #endif
--- include/libzelda64/SkinMatrix.h
+++ include/libzelda64/SkinMatrix.h
@@ -7,15 +7,15 @@
 #include <PR/gbi.h>
 
 extern void SkinMatrix_SetTranslate(MtxF* mf, float x, float y, float z);
-SYMBOL_VERSION_CONFIG(SkinMatrix_SetTranslate, 0x8008F82C, 0x80000180);
+SYMBOL_VERSION_CONFIG(SkinMatrix_SetTranslate, 0x8008F82C, 0x801397ac);
 
 extern void SkinMatrix_SetScale(MtxF* mf, float x, float y, float z);
-SYMBOL_VERSION_CONFIG(SkinMatrix_SetScale, 0x8008F4A8, 0x80000180);
+SYMBOL_VERSION_CONFIG(SkinMatrix_SetScale, 0x8008F4A8, 0x80139428);
 
 extern void SkinMatrix_MtxFMtxFMult(MtxF* mfB, MtxF* mfA, MtxF* dest);
-SYMBOL_VERSION_CONFIG(SkinMatrix_MtxFMtxFMult, 0x8008EDB8, 0x80000180);
+SYMBOL_VERSION_CONFIG(SkinMatrix_MtxFMtxFMult, 0x8008EDB8, 0x80138d38);
 
 extern Mtx* SkinMatrix_MtxFToNewMtx(struct GraphicsContext* gfxCtx, MtxF* src);
-SYMBOL_VERSION_CONFIG(SkinMatrix_MtxFToNewMtx, 0x8008FC98, 0x80000180);
+SYMBOL_VERSION_CONFIG(SkinMatrix_MtxFToNewMtx, 0x8008FC98, 0x80139c18);
 
 #endif
