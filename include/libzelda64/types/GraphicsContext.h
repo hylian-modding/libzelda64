9e4edc415325e966c96d5d394809793950d38edf
Drahsid <Drahsid@gmail.com>
2021-08-31 14:36:50 -0400

    Fix GraphicsContext

--- include/libzelda64/types/GraphicsContext.h
+++ include/libzelda64/types/GraphicsContext.h
@@ -12,31 +12,31 @@ typedef void (*GraphicsContextCallback)(struct GraphicsContext* graphicsContext,
 #ifdef GAME_OOT
 #ifdef GAME_VERSION_1_0
 typedef struct GraphicsContext {
-    /* 0x0000 */ struct Gfx* polyOpaBuffer;
-    /* 0x0004 */ struct Gfx* polyXluBuffer;
-    /* 0x0008 */ struct Gfx* polyLineOpaBuffer;
-    /* 0x000C */ struct Gfx* polyLineXluBuffer;
-    /* 0x0010 */ struct Gfx* polyOvlBuffer;
-    /* 0x0014 */ struct Gfx* gfxSave;
+    /* 0x0000 */ Gfx* polyOpaBuffer;
+    /* 0x0004 */ Gfx* polyXluBuffer;
+    /* 0x0008 */ Gfx* polyLineOpaBuffer;
+    /* 0x000C */ Gfx* polyLineXluBuffer;
+    /* 0x0010 */ Gfx* polyOvlBuffer;
+    /* 0x0014 */ Gfx* gfxSave;
     /* 0x0018 */ GraphicsMessage message;
     /* 0x0038 */ OSMesg msgBuff[8]; // This is the reply buffer (decomp's name is unclear)
-    /* 0x0058 */ struct OSMesgQueue* schedMesgQ;
+    /* 0x0058 */ OSMesgQueue* schedMesgQ;
     /* 0x005C */ OSMesgQueue queue; // This is the reply queue (decomp's name is unclear)
     /* 0x0078 */ OSScTask polyOpaTask;
     /* 0x00E0 */ OSScTask polyXluTask;
     /* 0x0148 */ OSScTask polyLineOpaTask;
-    /* 0x01B0 */ struct Gfx* workBuffer;
+    /* 0x01B0 */ Gfx* workBuffer;
     /* 0x01B4 */ TwoHeadGfxArena work;
     /* 0x01C4 */ uint8_t polyLineXluTask[104 - sizeof(Gfx*) - sizeof(TwoHeadGfxArena)];
     /* 0x0218 */ uint8_t polyOvlTask[104];
-    /* 0x0280 */ struct OSSched* sched;
+    /* 0x0280 */ OSSched* sched;
     /* 0x0284 */ struct OSViMode* viMode;
     /* 0x0288 */ TwoHeadGfxArena polyLineOpa;
     /* 0x0298 */ TwoHeadGfxArena polyLineXlu;
     /* 0x02A8 */ TwoHeadGfxArena overlay;
     /* 0x02B8 */ TwoHeadGfxArena polyOpa;
     /* 0x02C8 */ TwoHeadGfxArena polyXlu;
-    /* 0x02D8 */ uint32_t gfxPoolIdx; // source calls this frame_counter?
+    /* 0x02D8 */ uint32_t gfxPoolIdx;
     /* 0x02DC */ uint16_t* curFrameBuffer;
     /* 0x02E0 */ uint16_t* curRenderBuffer;
     /* 0x02E4 */ uint32_t viFeatures;
@@ -52,29 +52,30 @@ typedef struct GraphicsContext {
 #endif
 #elif defined GAME_MM /* GAME_OOT */
 typedef struct GraphicsContext {
-    /* 0x000 */ struct Gfx* polyOpaBuffer;
-    /* 0x004 */ struct Gfx* polyXluBuffer;
-    /* 0x008 */ struct Gfx* polyLineOpaBuffer;
-    /* 0x010 */ struct Gfx* polyOvlBuffer;
+    /* 0x000 */ Gfx* polyOpaBuffer;
+    /* 0x004 */ Gfx* polyXluBuffer;
+    /* 0x008 */ Gfx* polyLineOpaBuffer;
+    /* 0x00C */ Gfx* polyLineXluBuffer; // Assuming polyLineXlu using oot context
+    /* 0x010 */ Gfx* polyOvlBuffer;
     /* 0x014 */ char pad14[0x24];
     /* 0x038 */ GraphicsMessage message;
-    /* 0x058 */ OSMesg msgBuff[8]; // This is the reply buffer (decomp's name is unclear)
-    /* 0x05C */ struct OSMesgQueue* schedMesgQ;
+    /* 0x058 */ OSMesgQueue* schedMesgQ;
+    /* 0x05C */ OSMesgQueue queue;
     /* 0x074 */ char pad74[0x12C];
-    /* 0x1A0 */ struct Gfx* workBuffer;
+    /* 0x1A0 */ Gfx* workBuffer;
     /* 0x1A4 */ TwoHeadGfxArena work;
-    /* 0x1B4 */ struct Gfx* unk1B4;
-    /* 0x1B8 */ uint8_t polyLineXluTask[104 - sizeof(Gfx*) - sizeof(TwoHeadGfxArena)];
-    /* 0x1C8 */ uint8_t polyOvlTask[0xAC];
-    /* 0x274 */ struct OSSched* sched;
+    /* 0x1B4 */ Gfx* unk1B4;
+    /* 0x1B8 */ TwoHeadGfxArena unk1B8;
+    /* 0x1C8 */ uint8_t pad1C8[0xAC];
+    /* 0x274 */ OSViMode* unk274;
     /* 0x278 */ void* zbuffer;
     /* 0x27C */ char pad27C[0x1C];
     /* 0x298 */ TwoHeadGfxArena overlay;
     /* 0x2A8 */ TwoHeadGfxArena polyOpa;
     /* 0x2B8 */ TwoHeadGfxArena polyXlu;
-    /* 0x2C8 */ uint32_t gfxPoolIdx; // source calls this frame_counter?
+    /* 0x2C8 */ uint32_t gfxPoolIdx;
     /* 0x2CC */ uint16_t* curFrameBuffer;
-    /* 0x2D0 */ uint16_t* curRenderBuffer;
+    /* 0x2D0 */ uint16_t* curRenderBuffer; // possibly just the padding described in mm decomp
     /* 0x2D4 */ uint32_t viFeatures;
     /* 0x2D8 */ char gap2D8[0x3];
     /* 0x2DB */ uint8_t framebufferCounter;
